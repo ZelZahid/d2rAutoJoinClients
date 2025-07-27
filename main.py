4d5a67d66aee1622d53a4fbf6a89f54aed131b57
zel <zelgehai@gmail.com>
2025-07-27 14:03:26 -0700

    updated variables. Working Version

--- main.py
+++ main.py
@@ -49,103 +49,152 @@ def debug_mouse_position():
 game_name = "Zel"
 game_password = "123"
 game_number = 1
-numOfAccounts = 2
+numOfAccounts = 3
+
+client1_window_loc = 5
+#Enter Client Locations here:
+client2_window_loc = (2200, 20)  # Account 2
+client2_joingametab_loc = (2900, 90)
+client2_joingamename_loc = (2800, 136)
+client2_joingamepass_loc = (3000, 136)
+client2_saveandexit_loc = (2562, 362)  # Save and Exit for Account 2
+
+client3_window_loc = (3800, 1037)  # Account 3
+client3_joingametab_loc = (3550, 384)
+client3_joingamename_loc = (3450, 460)
+client3_joingamepass_loc = (3600, 460)
+client3_saveandexit_loc = (3200, 658)  # Save and Exit for Account 3
+
+#4th Account
+client4_window_loc = (3500, 20)   # Account 4
+client4_joingametab_loc = (3540, 84)
+client4_joingamename_loc = (3430, 131)
+client4_joingamepass_loc = (3618, 131)
+client4_saveandexit_loc = (3180, 348)  # Save and Exit for Account 4
+
+#5th Account
+client5_window_loc = (2000, 1020) # Account 5
+client5_joingametab_loc = (2880, 390)
+client5_joingamename_loc = (2880, 460)
+client5_joingamepass_loc = (3000, 460)
+client5_saveandexit_loc = (2562, 658)  # Save and Exit for Account 5
+
+#6th Account
+client6_window_loc = (-1874, 1000) # Account 6
+client6_joingametab_loc = (-965, 382)
+client6_joingamename_loc = (-965, 455)
+client6_joingamepass_loc = (-871, 455)
+client6_saveandexit_loc = (-871, 658)  # Save and Exit for Account 6
+
+#7th Account
+client7_window_loc = (-1700, 15)  # Account 7
+client7_joingametab_loc = (-1700, 90)
+client7_joingamename_loc = (-1800, 136)
+client7_joingamepass_loc = (-1700, 136)
+client7_saveandexit_loc = (-1710, 362)  # Save and Exit for Account 7
+
+#8th Account
+client8_window_loc = (-50, 1050)  # Account 8
+client8_joingametab_loc = (-50, 90)
+client8_joingamename_loc = (-150, 136)
+client8_joingamepass_loc = (-50, 136)
+client8_saveandexit_loc = (-60, 362)  # Save and Exit for Account 8
 
 def on_press_f3():
     global game_number
     game_number_str = str(game_number)
     print("JOINING GAME:", game_name, game_number)
 
-    # Account 1
-    safe_click(2200, 20)
-    safe_click(2900, 90)
-    safe_click(2800, 136)
+    # Account 2
+    safe_click(client2_window_loc[0], client2_window_loc[1])
+    safe_click(client2_joingametab_loc[0], client2_joingametab_loc[1])
+    safe_click(client2_joingamename_loc[0], client2_joingamename_loc[1])
     safe_hotkey('ctrl', 'a')
     safe_press('backspace')
     safe_write(game_name + game_number_str)
-    safe_click(3000, 136)
+    safe_click(client2_joingamepass_loc[0], client2_joingamepass_loc[1])
     safe_hotkey('ctrl', 'a')
     safe_press('backspace')
     safe_write(game_password)
     safe_press('enter')
 
     if numOfAccounts >= 3:
-        # Account 2
-        safe_click(3500, 925)
-        safe_click(3550, 384)
-        safe_click(3450, 460)
+        # Account 3
+        safe_click(client3_window_loc[0], client3_window_loc[1])
+        safe_click(client3_joingametab_loc[0], client3_joingametab_loc[1])
+        safe_click(client3_joingamename_loc[0], client3_joingamename_loc[1])
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_name + game_number_str)
-        safe_click(3600, 460)
+        safe_click(client3_joingamepass_loc[0], client3_joingamepass_loc[1])
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_password)
         safe_press('enter')
 
     if numOfAccounts >= 5:
-        # Account 3
-        safe_click(3500, 20)
-        safe_click(3553, 90)
-        safe_click(3450, 136)
+        # Account 4
+        safe_click(client4_window_loc[0], client4_window_loc[1])
+        safe_click(client4_joingametab_loc[0], client4_joingametab_loc[1])
+        safe_click(client4_joingamename_loc[0], client4_joingamename_loc[1] )
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_name + game_number_str)
-        safe_click(3650, 136)
+        safe_click(client4_joingamepass_loc[0], client4_joingamepass_loc[1])
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_password)
         safe_press('enter')
 
-        # Account 4
-        safe_click(2000, 1020)
-        safe_click(2880, 390)
-        safe_click(2880, 460)
+        # Account 5
+        safe_click(client5_window_loc[0], client5_window_loc[1])
+        safe_click(client5_joingametab_loc[0], client5_joingametab_loc[1])
+        safe_click(client5_joingamename_loc[0], client5_joingamename_loc[1])
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_name + game_number_str)
-        safe_click(3000, 460)
+        safe_click(client5_joingamepass_loc[0], client5_joingamepass_loc[1] )
         safe_hotkey('ctrl', 'a')
         safe_press('backspace')
         safe_write(game_password)
         safe_press('enter')
 
         if numOfAccounts >= 7:
-            # Account 5
-            safe_click(-1874, 1000)
-            safe_click(-965, 382)
-            safe_click(-965, 455)
+            # Account 6
+            safe_click(client6_window_loc[0], client6_window_loc[1])
+            safe_click(client6_joingametab_loc[0], client6_joingametab_loc[1])
+            safe_click(client6_joingamename_loc[0], client6_joingamename_loc[1])
             safe_hotkey('ctrl', 'a')
             safe_press('backspace')
             safe_write(game_name + game_number_str)
-            safe_click(-871, 455)
+            safe_click(client6_joingamepass_loc[0], client6_joingamepass_loc[1])
             safe_hotkey('ctrl', 'a')
             safe_press('backspace')
             safe_write(game_password)
             safe_press('enter')
 
-            # Account 6
-            safe_click(-1700, 15)
-            safe_click(-970, 85)
-            safe_click(-970, 136)
+            # Account 7
+            safe_click(client7_window_loc[0], client7_window_loc[1])
+            safe_click(client7_joingametab_loc[0], client7_joingametab_loc[1])
+            safe_click(client7_joingamename_loc[0], client7_joingamename_loc[1])
             safe_hotkey('ctrl', 'a')
             safe_press('backspace')
             safe_write(game_name + game_number_str)
-            safe_click(-857, 136)
+            safe_click(client7_joingamepass_loc[0], client7_joingamepass_loc[1])
             safe_hotkey('ctrl', 'a')
             safe_press('backspace')
             safe_write(game_password)
             safe_press('enter')
 
             if numOfAccounts == 8:
-                # Account 7
-                safe_click(-50, 1050)
-                safe_click(-295, 385)
-                safe_click(-392, 455)
+                # Account 8
+                safe_click(client8_window_loc[0], client8_window_loc[1])
+                safe_click(client8_joingametab_loc[0], client8_joingametab_loc[1])
+                safe_click(client8_joingamename_loc[0], client8_joingamename_loc[1])
                 safe_hotkey('ctrl', 'a')
                 safe_press('backspace')
                 safe_write(game_name + game_number_str)
-                safe_click(-200, 455)
+                safe_click(client8_joingamepass_loc[0], client8_joingamepass_loc[1])
                 safe_hotkey('ctrl', 'a')
                 safe_press('backspace')
                 safe_write(game_password)
@@ -184,31 +233,31 @@ def on_press_f4():
 
 def on_press_f5():
     print("EXIT init()")
-    safe_click(2200, 20)
+    safe_click(client2_window_loc[0], client2_window_loc[1])
     safe_press('esc')
-    safe_click(2562, 362)
+    safe_click(client2_saveandexit_loc[0], client2_saveandexit_loc[1])
     if numOfAccounts >= 3:
-        safe_click(3500, 925)
+        safe_click(client3_window_loc[0], client3_window_loc[1])
         safe_press('esc')
-        safe_click(3200, 658)
+        safe_click(client3_saveandexit_loc[0], client3_saveandexit_loc[1])
     if numOfAccounts >= 5:
-        safe_click(3500, 20)
+        safe_click(client4_window_loc[0], client4_window_loc[1])
         safe_press('esc')
-        safe_click(3200, 360)
-        safe_click(2000, 1020)
+        safe_click(client4_saveandexit_loc[0], client4_saveandexit_loc[1])
+        safe_click(client5_window_loc[0], client5_window_loc[1])
         safe_press('esc')
-        safe_click(2560, 664)
+        safe_click(client5_saveandexit_loc[0], client5_saveandexit_loc[1])
         if numOfAccounts >= 7:
-            safe_click(-1874, 1000)
+            safe_click(client6_window_loc[0], client6_window_loc[1])
             safe_press('esc')
-            safe_click(-1284, 660)
-            safe_click(-1700, 15)
+            safe_click(client6_saveandexit_loc[0], client6_saveandexit_loc[1])
+            safe_click(client7_window_loc[0], client7_window_loc[1])
             safe_press('esc')
-            safe_click(-1284, 360)
+            safe_click(client7_saveandexit_loc[0], client7_saveandexit_loc[1])
             if numOfAccounts == 8:
-                safe_click(-50, 1050)
+                safe_click(client8_window_loc[0], client8_window_loc[1])
                 safe_press('esc')
-                safe_click(-648, 659)
+                safe_click(client8_saveandexit_loc[0], client8_saveandexit_loc[1])
     safe_click(-350, 20)
     #safe_click(960, 505) #replace with move mouse pos
     pyautogui.moveTo(960, 505,duration=0.05) #smoother mouse movement
